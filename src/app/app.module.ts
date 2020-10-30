05cc277132932873e3bff696652d669f16b8e372
p3cunha <pcls_@hotmail.com>
2020-10-29 21:42:21 -0300

    note-list started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoteListComponent } from './note-list/note-list.component';
+import { NoteListComponent } from './pages/note-list/note-list.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteCardComponent } from './note-card/note-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NoteListComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
