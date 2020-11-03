c8a3d5fab6b98b8c2c274ba9b4f925982ce8dc0a
p3cunha <pcls_@hotmail.com>
2020-11-03 14:30:43 -0300

    initial crud service

--- src/app/shared/notes.service.ts
+++ src/app/shared/notes.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
 
+import { Note } from './note.module';
+
 @Injectable({
   providedIn: 'root'
 })
 export class NotesService {
 
+  notes: Note[] = new Array<Note>();
+
   constructor() { }
+
+  get(id: number){
+    return this.notes[id]
+  }
+
+  getId(note: Note){
+    return this.notes.indexOf(note)
+  }
+
+  add(note: Note){
+    let noteLength = this.notes.push(note) // push note into var
+    let index = noteLength -1 // id - index
+    return index
+  }
+
+  update(id: number, title: string, body: string){
+    let note = this.notes[id]
+    note.title = title;
+    note.body = body
+  }
+
+  delete(id: number){
+    this.notes.splice(id, 1)
+  }
+
 }
