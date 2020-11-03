fbfa88d816c676a95031e6a9efec98386b0b76be
p3cunha <pcls_@hotmail.com>
2020-11-03 14:09:18 -0300

    add form-details & validation CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { NoteListComponent } from './pages/note-list/note-list.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteDetailsComponent } from './pages/note-details/note-details.component';
 
 const routes: Routes = [
   {  path: '', component: MainLayoutComponent, children: [
-    { path: '', component: NoteListComponent }  ] //inject noteList in MainLayout by routing
+    { path: '', component: NoteListComponent }, //inject noteList in MainLayout by routing
+    { path: ':id', component: NoteDetailsComponent} ] 
   }
 ];
 
---
+++ src/app/pages/note-details/note-details.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Note } from '../shared/note.module';
+
+@Component({
+  selector: 'app-note-details',
+  templateUrl: './note-details.component.html',
+  styleUrls: ['./note-details.component.scss']
+})
+export class NoteDetailsComponent implements OnInit {
+
+  note: Note
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.note = new Note(); //instantiate note to init correctly
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form)
+  }
+}
